4d55142efad33e1722f6b175366bae4713b60dbf
Vitor Henrique <vhpsantos@gmail.com>
2023-07-18 11:00:10 -0300

    add icon.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
+      <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous"/>
       <Script id='gtm-google-tag'>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
