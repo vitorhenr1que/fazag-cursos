19163b7bc1f82beeb0bd12feeadf1b8cbf19ad27
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-11-12 21:35:21 -0300

    alterado link do matricular

---
+++ src/app/matriculas/page.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from './style.module.scss'
+
+export default function Matriculas() {
+    return (
+        <div className={styles.container}>
+            <h1>Inscrições FAZAG {`${new Date().getFullYear()}`}</h1>
+
+            <div className={styles.containerLinks}>
+                <h3>Formas de Ingresso</h3>
+                <div className={styles.linkContainer}>
+                <Link className={styles.link} target="_blank" href={'https://portais.qualinfonet.com.br/fazag/portalcandidato.vestibular.php?acao=directclean&vest_codigo=20261A'}>Vestibular Online</Link>
+                <Link className={styles.link} target="_blank" href={'https://portais.qualinfonet.com.br/fazag/portalcandidato.vestibular.php?acao=directclean&vest_codigo=20261E'}>Nota do ENEM</Link>
+                <Link className={styles.link} target="_blank" href={'https://portais.qualinfonet.com.br/fazag/portalcandidato.vestibular.php?acao=directclean&vest_codigo=20261T'}>Transferência Externa</Link>
+                <Link className={styles.link} target="_blank" href={'https://portais.qualinfonet.com.br/fazag/portalcandidato.vestibular.php?acao=directclean&vest_codigo=20261S'}>Segunda Graduação</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
